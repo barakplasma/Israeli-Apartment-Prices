76837ebfa1860f605deda94181dfd3eb1efdddcb
Michael Salaverry <michaels@wix.com>
2017-12-27 23:02:13 +0200

    cleaned up

--- index.js
+++ index.js
@@ -7,16 +7,6 @@ const yad2searchUrl = `/feed/2/2/`;
 const query = `location_type=3&area=2&fromRooms=1&toRooms=1&fromPrice=2000&toPrice=3000&airConditioner=1&renovated=1&priceOnly=1&imgOnly=1`;
 const url = baseUrl + yad2searchUrl + query;
 
-const flipHebrew = (hebrew) => {
-    if(_.isString(hebrew)){
-        return _.reverse(hebrew.split('')).join('');
-    } else if (!_.isNil(hebrew)) {
-        return hebrew;
-    } else {
-        return '';
-    }
-};
-
 osmosis.config('follow', 0)
 
 osmosis.config('user_agent', `Mozilla/5.0 (iPhone; CPU iPhone OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5376e Safari/8536.25`)
@@ -36,26 +26,6 @@ osmosis
         'sqm': '.ad_page_item_info_bar_item_title:nth(2)'
     })
     .data(function (listing) {
-        // do something with listing data
-
-        // const flipped = _.mapValues(listing, flipHebrew);
-        // const mostlyFlipped = _(flipped)
-        //     .set('price', 
-        //         _.parseInt(flipHebrew(flipped.price)
-        //         .split(',')
-        //         .join('')
-        //         .slice(0,-2))
-        //     )
-        //     .set('latlon', 
-        //         flipHebrew(flipped.latlon)
-        //         .match(/\d\d[^\&]+/)[0]
-        //     )
-        //     .set('sqm', 
-        //         _.parseInt(flipHebrew(flipped.sqm)
-        //         .match(/\d+/)[0])
-        //     )
-        //     .value();
-        // console.log(mostlyFlipped);
         if(_.has(listing,'latlon')){
         const extractedLocation = listing.latlon.match(/\d\d[^\&]+/);
         if(_.has(extractedLocation, [0]) && !_.isNil(extractedLocation[0])) {
