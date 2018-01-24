945d9a2dbf275876bdf74039038f1dbf2fb4fae0
Michael Salaverry <michaels@wix.com>
2018-01-24 16:09:54 +0200

    changed location into function and adjusted types of URLs

--- index.js
+++ index.js
@@ -1,32 +1,30 @@
 const osmosis = require('osmosis');
 const _ = require('lodash');
+const {URL} = require('url');
 const fs = require('fs');
 
 const Database = require('better-sqlite3');
 const db = new Database('rentals.db');
 const stmt = db.prepare('CREATE TABLE IF NOT EXISTS rentals (url TEXT PRIMARY KEY, price INTEGER, latlon TEXT)').run();
 
-const baseUrl = `https://m.yad2.co.il`;
-const yad2searchUrl = `/feed/2/2/`;
-const minRooms = 1;
-const maxRooms = 2;
-const minPrice = 1500;
-const maxPrice = 3393;
-const priceAndSize = `&fromRooms=${minRooms}&toRooms=${maxRooms}&fromPrice=${minPrice}&toPrice=${maxPrice}`;
-const location = `location_type=3&area=47`;
-const otherOptions = `&priceOnly=1&imgOnly=1`;
-const query = location + priceAndSize + otherOptions;
-const url = baseUrl + yad2searchUrl + query;
+const createYad2Link = (location = `location_type=3&area=47`, minPrice = 1500, maxPrice = 3393, minRooms = 1, maxRooms = 2) => {
+    const baseUrl = `https://m.yad2.co.il`;
+    const yad2searchUrl = `/feed/2/2/`;
+    const otherOptions = `&priceOnly=1&imgOnly=1`;
+    const priceAndSize = `&fromRooms=${minRooms}&toRooms=${maxRooms}&fromPrice=${minPrice}&toPrice=${maxPrice}`;
+    const query = location + priceAndSize + otherOptions;
+    const url = new URL(baseUrl + yad2searchUrl + query).href;
+    return url;
+}
 
 osmosis.config('follow', 0)
 
 osmosis.config('user_agent', `Mozilla/5.0 (iPhone; CPU iPhone OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5376e Safari/8536.25`)
 
 const handleErrors = (err) => {if (err) throw err;}
-fs.writeFileSync('./housing.json','[', handleErrors);
 
 osmosis
-    .get(url)
+    .get(createYad2Link())
     .paginate('#ad_feed_pagination_item_next_icon')
     .follow('[id^=ad_feed_ad_item_link]')
     .set({
@@ -40,7 +38,7 @@ osmosis
         'sqm': '.ad_page_item_info_bar_item_title:nth(2)'
     })
     .then(function(context, data, next) {
-        data.url = context.__location.url.href;
+        data.url = new URL(context.__location.url.href);
         next(context, data);
     })
     .data(function (listing) {
@@ -54,7 +52,7 @@ osmosis
             _.set(listing,'price', price);
             // _.set(listing,'price', normalizeValue(price, minPrice, maxPrice));
             // console.log(1, listing);
-            const newRow = db.prepare(`INSERT OR REPLACE INTO rentals (url, price, latlon) VALUES ("${listing.url}", ${listing.price}, "${listing.latlon}")`).run();
+            const newRow = db.prepare(`INSERT OR REPLACE INTO rentals (url, price, latlon) VALUES ("${listing.url.href}", ${listing.price}, "${listing.latlon}")`).run();
         }}
     })
     .log(console.log)
@@ -62,6 +60,8 @@ osmosis
     .debug(console.log)
     .then(() =>{
         const allDBRows = db.prepare(`SELECT * from rentals ORDER BY price desc`).all()
+        
+        fs.writeFileSync('./housing.json','[', handleErrors);
 
         allDBRows.forEach(listing => fs.appendFile('./housing.json', `[${listing.latlon},${listing.price},"${listing.url}"],`, {encoding: 'utf8'}, handleErrors));
 
